2f0438833c32e6d4a464c12f9c03d868e8e5e8ef
Wonsik Jeong <wsjeong21@hotmail.com>
2015-10-26 21:48:38 -0400

    behind logic is complete

--- javascript.js
+++ javascript.js
@@ -6,43 +6,77 @@ $(document).ready( function(){
 // reader friendly
 
 
-// created 52cards
-  var suits=['H', 'D', 'C', 'S'];
-  var cards=[];
+
+
+
+
+setUp={
+  cards: [],
+  playerOneHand: [],
+  playerTwoHand: [],
+  // create 52 cards
+  createCards: function(){
+    var suits=['H', 'D', 'C', 'S'];
     for (var i=1; i<14; i++){
       for (var j=0; j<4; j++){
-        cards.push(i+suits[j]);
+        this.cards.push(i+suits[j]);
       }
     }
 
-console.log(cards);
+  },
+  // shuffles the deck of 52 cards
+  shuffle: function(){
 
-// create a shuffle function
-function shuffle(){
-  var cardArray=cards.slice();
-  var currentIndex = cardArray.length;
-    var tempValue, randomIndex;
-    while (0 !== currentIndex) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        tempValue = cardArray[currentIndex];
-        cardArray[currentIndex] = cardArray[randomIndex];
-        cardArray[randomIndex] = tempValue;
+      var cardArray=this.cards;
+      var currentIndex = cardArray.length;
+        var tempValue, randomIndex;
+        while (0 !== currentIndex) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            tempValue = cardArray[currentIndex];
+            cardArray[currentIndex] = cardArray[randomIndex];
+            cardArray[randomIndex] = tempValue;
+        }
+
+    },
+  // give two players 26 cards each
+  splitTwo: function(){
+      this.playerOneHand=this.cards.splice(0,26);
+      this.playerTwoHand=this.cards;
+
+
+  }
+
+
+  }
+
+var compare = {
+  pullACard: function(){
+    var playerOneCard=setUp.playerOneHand.pop();
+    var playerTwoCard=setUp.playerTwoHand.pop();
+    console.log( setUp.playerOneHand );
+    console.log(setUp.playerTwoHand);
+    if(parseInt(playerOneCard)>=parseInt(playerTwoCard)){
+      console.log("player one wins");
+      setUp.playerOneHand.unshift(playerOneCard);
+      setUp.playerOneHand.unshift(playerTwoCard);
+
     }
-  return cardArray;
+    if(parseInt(playerOneCard)<parseInt(playerTwoCard)){
+      console.log("player two wins");
+      setUp.playerTwoHand.unshift(playerOneCard);
+      setUp.playerTwoHand.unshift(playerTwoCard);
+
+    }
+  }
 }
 
-console.log(shuffle());
-
-// splits the deck into two players hands
-function splitTwo(deck){
-  var playerOneHand=deck.splice(0,26);
-  var playerTwoHand=deck;
-
-}
-
-console.log(splitTwo(shuffle()));
-
+setUp.createCards();
+setUp.shuffle();
+setUp.splitTwo();
+console.log(setUp.playerOneHand);
+console.log(setUp.playerTwoHand);
+compare.pullACard();
 
 
 
